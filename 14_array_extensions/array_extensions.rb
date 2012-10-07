6598d6f77add4b8ba60b70251745ff75f8a1c779
Andrey <andrice@i.ua>
2012-10-07 10:47:31 +0300

    15 - refactored (ArrayExtensions module added)

--- 14_array_extensions/array_extensions.rb
+++ 14_array_extensions/array_extensions.rb
@@ -1,4 +1,4 @@
-class Array
+module ArrayExtensions
   def sum
     self.empty? ? 0 : self.reduce(0) { |s, item| s + item }
   end
@@ -8,6 +8,10 @@ class Array
   end
 
   def square!
-    self.empty? ? self : self.map! { |item| item**2 }
+    replace(square)
   end
+end
+
+class Array
+  include ArrayExtensions
 end
