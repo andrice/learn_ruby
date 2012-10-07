3aa6beb7c870880cb72f060962b9a654d9060286
Andrey <andrice@i.ua>
2012-10-07 08:56:31 +0300

    15 - Array class had been extended due to the task, all test had passed

---
+++ 14_array_extensions/array_extensions.rb
@@ -0,0 +1,13 @@
+class Array
+  def sum
+    self.empty? ? 0 : self.inject(0) { |s, item| s += item }
+  end
+
+  def square
+    self.empty? ? self : self.map { |item| item**2 }
+  end
+
+  def square!
+    self.empty? ? self : self.map! { |item| item**2 }
+  end
+end
